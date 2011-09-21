ef80e3bc1dcce7a4925e766088e8dcf2b8f65481
Daniel Gidman <danatcofo@users.noreply.github.com>
2011-09-21 14:23:38 +0000

    implemented  with thanks to Kabwla.Phone

--- SerializableExtraTypes/SerializableExtraTypeAttribute.cs
+++ SerializableExtraTypes/SerializableExtraTypeAttribute.cs
@@ -8,18 +8,8 @@ namespace System.Xml.Serialization
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = true)]
     public class SerializableExtraTypeAttribute : Attribute
     {
-        public SerializableExtraTypeAttribute(Type t) 
-            : base() { _Types = new Type[] { t }; }
-        public SerializableExtraTypeAttribute(Type t1, Type t2) 
-            : base() { _Types = new Type[] { t1, t2 }; }
-        public SerializableExtraTypeAttribute(Type t1, Type t2, Type t3) 
-            : base() { _Types = new Type[] { t1, t2, t3, }; }
-        public SerializableExtraTypeAttribute(Type t1, Type t2, Type t3, Type t4) 
-            : base() { _Types = new Type[] { t1, t2, t3, t4, }; }
-        public SerializableExtraTypeAttribute(Type t1, Type t2, Type t3, Type t4, Type t5) 
-            : base() { _Types = new Type[] { t1, t2, t3, t4, t5, }; }
-        public SerializableExtraTypeAttribute(Type t1, Type t2, Type t3, Type t4, Type t5, Type t6) 
-            : base() { _Types = new Type[] { t1, t2, t3, t4, t5, t6, }; }
+        public SerializableExtraTypeAttribute(params Type[] types)
+            : base() { _Types = types.Where(t => t != null).ToArray(); }
 
         private IEnumerable<Type> _Types = null;
 
